da3d68d6bbe72c949e249a42f3850564cec72410
audidas <audidas@naver.com>
2020-05-19 15:50:58 +0900

    상속
    상속까지

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -2,60 +2,45 @@
 
 namespace OOP
 {
-    // 참조
-    class Knight
+    // OOP(은닉성 / 상속성 / 다형성)
+    class Player // 부모 / 기반 
     {
-        // 필드
         static public int counter = 1; // 오로지 1개만 존재
-
         public int id;
         public int hp;
         public int attack;
-        static public void Test()
-        {
-            Console.WriteLine(counter);
-        }
-        static public Knight CreateKnight()
-        {
-            Knight knight = new Knight();
-            knight.hp = 100;
-            knight.attack = 1;
-            return knight;
-        }
-        public Knight()
-        {
-            id = counter;
-            counter++;
-
-            hp = 100;
-            attack = 10;
-            Console.WriteLine("생성자 호출");
-        }
-        public Knight Clone()
-        {
-            Knight knight = new Knight();
-            knight.hp = hp;
-            knight.attack = attack;
-            return knight;
-        }
         public void Move()
         {
-            System.Console.WriteLine("Knight Move");
+            System.Console.WriteLine("Player Move");
         }
-
         public void Attack()
         {
-            System.Console.WriteLine("Knight Attack");
+            System.Console.WriteLine("Player Attack");
         }
     }
-    // 복사
+
+    class Mage : Player
+    {
+
+    }
+    class Archer : Player
+    {
+
+    }
+    class Knight : Player // 자식 / 파생
+    {
+        public void Stun()
+        {
+
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
-          Knight knight = Knight.CreateKnight(); // static 
-          knight.Move(); // 일반
+            Knight knight = new Knight();
+            knight.Attack();
         }
     }
 }
