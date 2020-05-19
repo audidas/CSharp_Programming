5110f0bda488f870e08c938ec88add36da1c836a
audidas <audidas@naver.com>
2020-05-19 14:34:13 +0900

    복사와 참조
    복사와 참조

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -2,10 +2,18 @@
 
 namespace OOP
 {
+    // 참조
     class Knight
     {
         public int hp;
         public int attack;
+        public Knight Clone()
+        {
+            Knight knight = new Knight();
+            knight.hp = hp;
+            knight.attack = attack;
+            return knight;
+        }
         public void Move()
         {
             System.Console.WriteLine("Knight Move");
@@ -16,11 +24,40 @@ namespace OOP
             System.Console.WriteLine("Knight Attack");
         }
     }
+    // 복사
+    struct Mage
+    {
+        public int hp;
+        public int attack;
+    }
     class Program
     {
+        static void KillMage(Mage mage)
+        {
+            mage.hp = 0;
+        }
+        static void KillKnight(Knight knight)
+        {
+            knight.hp = 0;
+        }
         static void Main(string[] args)
         {
+            Mage mage = new Mage();
+            mage.hp = 100;
+            mage.attack = 50;
+
+            Mage mage2 = mage;
+            mage2.hp = 0;
+
             Knight knight = new Knight();
+
+            knight.hp = 100;
+            knight.attack = 10;
+            // KillKnight(knight);
+
+            Knight knight2 = knight.Clone();
+            knight2.hp = 0;
+
         }
     }
 }
