18327f0c92a538e80369206c5d17a1c0f741673c
audidas <audidas@naver.com>
2020-05-19 14:16:26 +0900

    CSharp 객체지향 시작
    시작

---
+++ OOP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP
+{
+    class Knight
+    {
+        public int hp;
+        public int attack;
+        public void Move()
+        {
+            System.Console.WriteLine("Knight Move");
+        }
+
+        public void Attack()
+        {
+            System.Console.WriteLine("Knight Attack");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight knight = new Knight();
+        }
+    }
+}
