bc8833eb9c9aa123fdf8bce4dd142f215966e205
audidas <audidas@naver.com>
2020-05-20 18:21:20 +0900

    배열 시작

---
+++ DataStructure/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DataStructure
+{
+    class Player
+    {
+
+    }
+    class Moster
+    {
+
+    }
+    class Program
+    {
+        Player player;
+        Moster monster;
+        static void Main(string[] args)
+        {
+            //배열
+            int[] scores = new int[5];
+            
+            // 0 1 2 3 4
+            scores[0] =10;
+            scores[1] =20;
+            scores[2] =30;
+            scores[3] =40;
+            scores[4] =50;
+
+        }
+    }
+}
