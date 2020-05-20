7a539bcc4e4584f4f05bf4a435b0cd7c5be7e65e
audidas <audidas@naver.com>
2020-05-20 16:01:33 +0900

    다형성
    다형성 virtual 과 override

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -8,20 +8,38 @@ namespace OOP
     {
         protected int hp;
         protected int attack;
+        public virtual void Move()
+        {
+            Console.WriteLine("Player 이동!");
+        }
     }
     class Knight : Player
     {
+        public sealed override void Move()
+        {
+            base.Move();
+            Console.WriteLine("걸어서 이동");
+        }
+    }
 
+    class SuperKnight : Knight
+    {
+        
     }
     class Mage : Player
     {
         public int mp;
+        public override void Move()
+        {
+            Console.WriteLine("순간이동");
+        }
     }
 
     class Program
     {
         static void EnterGame(Player player)
         {
+            player.Move();
             Mage mage = player as Mage;
             if (mage != null)
             {
@@ -33,7 +51,8 @@ namespace OOP
             Knight knight = new Knight();
             Mage mage = new Mage();
 
-            EnterGame(mage);
+            knight.Move();
+            //EnterGame(mage);
 
         }
     }
