05b53025424087228f70f13102b179afcd8fbd9e
audidas <audidas@naver.com>
2020-05-21 13:58:32 +0900

    배열

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -17,15 +17,26 @@ namespace DataStructure
         static void Main(string[] args)
         {
             //배열
-            int[] scores = new int[5];
-            
-            // 0 1 2 3 4
-            scores[0] =10;
-            scores[1] =20;
-            scores[2] =30;
-            scores[3] =40;
-            scores[4] =50;
+            int[] scores = new int[5] { 10, 20, 30, 40, 50 };
+            int[] scores2 = scores;
 
+            scores2[0] = 9999;
+
+            // 0 1 2 3 4
+            // scores[0] = 10;
+            // scores[1] = 20;
+            // scores[2] = 30;
+            // scores[3] = 40;
+            // scores[4] = 50;
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine(scores[i]);
+            }
+
+            foreach (int score in scores)
+            {
+                Console.WriteLine(score);
+            }
         }
     }
 }
