8f834517607262a1bed4dacb5a821e5ec15d7525
audidas <audidas@naver.com>
2020-05-21 14:58:46 +0900

    배열 연습문제

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -12,28 +12,100 @@ namespace DataStructure
     }
     class Program
     {
-        Player player;
-        Moster monster;
+
+        static int GetHighestScore(int[] scores)
+        {
+            if (scores.Length < 1)
+            {
+                return 0;
+            }
+            else
+            {
+                int high = 0;
+                foreach (int score in scores)
+                {
+                    if (score > high)
+                    {
+                        high = score;
+                    }
+                }
+                return high;
+            }
+
+        }
+
+        static int GetAverageScore(int[] scores)
+        {
+            if (scores.Length < 1)
+            {
+                return 0;
+            }
+            else
+            {
+                int avg = 0;
+                foreach (int score in scores)
+                {
+                    avg += score;
+                }
+                return avg / scores.Length;
+            }
+
+        }
+
+        static int GetIndexOf(int[] scores, int value)
+        {
+            if (scores.Length < 1)
+            {
+                return 0;
+            }
+            else
+            {
+                for (int i = 0; i < scores.Length; i++)
+                {
+                    if (scores[i] == value)
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+
+        }
+
+        static int[] Sort(int[] scores)
+        {
+            if (scores.Length < 1)
+            {
+                return null;
+            }
+            int temp = 0;
+            for (int i = 0; i < scores.Length; i++)
+            {
+                for (int j = i; j < scores.Length; j++)
+                {
+                    if (scores[i] > scores[j])
+                    {
+                        temp = scores[j];
+                        scores[j] = scores[i];
+                        scores[i] = temp;
+                    }
+                }
+            }
+            return scores;
+
+        }
         static void Main(string[] args)
         {
             //배열
-            int[] scores = new int[5] { 10, 20, 30, 40, 50 };
-            int[] scores2 = scores;
+            int[] scores = new int[7] { 10, 10, 30, 30, 10,27,5 };
 
-            scores2[0] = 9999;
+            Console.WriteLine(GetHighestScore(scores));
+            Console.WriteLine(GetAverageScore(scores));
+            Console.WriteLine(GetIndexOf(scores, 12));
 
-            // 0 1 2 3 4
-            // scores[0] = 10;
-            // scores[1] = 20;
-            // scores[2] = 30;
-            // scores[3] = 40;
-            // scores[4] = 50;
-            for (int i = 0; i < scores.Length; i++)
-            {
-                Console.WriteLine(scores[i]);
-            }
+            int[] scores2 = Sort(scores);
 
-            foreach (int score in scores)
+            foreach (int score in scores2)
             {
                 Console.WriteLine(score);
             }
