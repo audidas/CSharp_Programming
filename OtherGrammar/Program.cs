56c836999df6ad1b15f406728c2ee09c0903a7a2
audidas <audidas@naver.com>
2020-05-22 17:29:44 +0900

    Property

--- OtherGrammar/Program.cs
+++ OtherGrammar/Program.cs
@@ -5,49 +5,21 @@ namespace OtherGrammar
 {
     class Program
     {
-
-        abstract class Monster
+        class Knight
         {
-            public abstract void Shout();
 
-        }
-
-        interface IFlyable
-        {
-            void Fly();
-        }
-        class Orc : Monster
-        {
-            public override void Shout()
+            // 객체지향 -> 은닉성
+            public int Hp
             {
-                Console.WriteLine("록타르 오가르!");
-            }
-        }
-        class FlyableOrc : Orc, IFlyable
-        {
-            public void Fly()
-            {
-
-            }
-        }
-
-
-        class Skeleton : Monster
-        {
-            public override void Shout()
-            {
-                System.Console.WriteLine("꾸에에엑!");
-            }
-        }
-
-        static void DoFly(IFlyable flyable)
-        {
-            flyable.Fly();
+                get;
+                set;
+            } = 100;
         }
         static void Main(string[] args)
         {
-            FlyableOrc orc = new FlyableOrc();
-            DoFly(orc);
+            // 프로퍼티
+            Knight knight = new Knight();
+            int hp = knight.Hp;
         }
     }
 }
