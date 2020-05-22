34438f02e782f9db38463d95ed4a0ef731264864
audidas <audidas@naver.com>
2020-05-22 17:54:03 +0900

    Delegate
    대리자

--- OtherGrammar/Program.cs
+++ OtherGrammar/Program.cs
@@ -5,21 +5,42 @@ namespace OtherGrammar
 {
     class Program
     {
-        class Knight
-        {
+        // 업체 사장 - 사장님의 비서
+        // 우리의 연락처 / 용건 거꾸로 -> 연락을 달라고
 
-            // 객체지향 -> 은닉성
-            public int Hp
-            {
-                get;
-                set;
-            } = 100;
+        delegate int OnClicked();
+        // delegate -> 형식은 형식인데 , 함수 자체를 인자로 넘겨주는 형식
+        // 반환 : int 입력 void
+        //  OnClicked 이 delegate 형식의 이름이다!
+
+        // UI
+        static void ButtonPressed(OnClicked clickedFunction/* 함수 자체를 인자로 넘겨주고  */)
+        {
+            // PlayerAttack();
+            // 함수를 호출();
+            clickedFunction();
+        }
+
+        // [10,20,40,30,50]
+
+        static int TestDelegate()
+        {
+            System.Console.WriteLine("Hello delegate");
+            return 0;
+        }
+        static int TestDelegate2()
+        {
+            System.Console.WriteLine("Hello delegate2");
+            return 0;
         }
         static void Main(string[] args)
         {
-            // 프로퍼티
-            Knight knight = new Knight();
-            int hp = knight.Hp;
+            // delegate (대리자)
+            OnClicked clicked = new OnClicked(TestDelegate);
+            clicked += TestDelegate2;
+
+            ButtonPressed(clicked);
+
         }
     }
 }
