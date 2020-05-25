78dad857ed5562d974c7143bf9310fe9e9dc5b5c
audidas <audidas@naver.com>
2020-05-25 16:24:33 +0900

    Lamda

--- OtherGrammar/InputManager.cs
+++ OtherGrammar/InputManager.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 namespace OtherGrammar
 {
+    
     // Observer Pattern
     public class InputManager
     {
--- OtherGrammar/Program.cs
+++ OtherGrammar/Program.cs
@@ -3,21 +3,59 @@ using System.Collections.Generic;
 
 namespace OtherGrammar
 {
+    enum ItemType
+    {
+        Weapon,
+        Armor,
+        Amulet,
+        Ring
+    }
+
+    enum Rarity
+    {
+        Normal,
+        Uncommon,
+        Rare
+    }
+
+
+    class Item
+    {
+        public ItemType ItemType;
+        public Rarity Rarity;
+    }
     class Program
     {
-        static void OnInputTest()
+        static List<Item> items = new List<Item>();
+
+        delegate bool ItemSelector(Item item);
+        static Item FindItem(ItemSelector selector)
         {
-            System.Console.WriteLine("Input Received");
+            foreach (var item in items)
+            {
+                if (selector(item))
+                {
+                    return item;
+                }
+            }
+            return null;
         }
+
         static void Main(string[] args)
         {
-            InputManager inputManager = new InputManager();
+            items.Add(new Item() { ItemType = ItemType.Weapon, Rarity = Rarity.Normal });
+            items.Add(new Item() { ItemType = ItemType.Armor, Rarity = Rarity.Uncommon });
+            items.Add(new Item() { ItemType = ItemType.Ring, Rarity = Rarity.Rare });
 
-            inputManager.InputKey += OnInputTest;
-            while (true)
+            ItemSelector selector = new ItemSelector((Item _item) => { return _item.Rarity == Rarity.Rare; });
+
+            // 무명 함수 / 익명 함수
+            Item item = FindItem((Item _item) =>
             {
-                inputManager.Update();
-            }
+                return _item.ItemType == ItemType.Weapon;
+            });
+
         }
+
     }
 }
