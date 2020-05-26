9921638fac3000b91a751d2b9f55d97898bf42f3
audidas <audidas@naver.com>
2020-05-26 15:52:34 +0900

    Exception

--- OtherGrammar/Program.cs
+++ OtherGrammar/Program.cs
@@ -3,57 +3,39 @@ using System.Collections.Generic;
 
 namespace OtherGrammar
 {
-    enum ItemType
-    {
-        Weapon,
-        Armor,
-        Amulet,
-        Ring
-    }
-
-    enum Rarity
-    {
-        Normal,
-        Uncommon,
-        Rare
-    }
-
-
-    class Item
-    {
-        public ItemType ItemType;
-        public Rarity Rarity;
-    }
     class Program
     {
-        static List<Item> items = new List<Item>();
-
-        //delegate Return MyFunc<T, Return>(T item);
-        static Item FindItem(Func<Item, bool> selector)
+        class TestException : Exception
         {
-            foreach (var item in items)
-            {
-                if (selector(item))
-                {
-                    return item;
-                }
-            }
-            return null;
-        }
 
+        }
         static void Main(string[] args)
         {
-            items.Add(new Item() { ItemType = ItemType.Weapon, Rarity = Rarity.Normal });
-            items.Add(new Item() { ItemType = ItemType.Armor, Rarity = Rarity.Uncommon });
-            items.Add(new Item() { ItemType = ItemType.Ring, Rarity = Rarity.Rare });
+            try
+            {
+                // 1. 0으로 나눌 다른
+                // 2. 잘못된 메모리를 참조 (null)
+                // 3. 오버플로우
+                // int a = 10;
+                // int b = 0;
+                // int result = a / b;
 
-            Func<Item, bool> selector = (Item _item) => { return _item.Rarity == Rarity.Rare; };
-            // delegate 를 직접 선언하지 않아도 , 이미 만들어진 애들이 존재한다
-            // -> 반환 타입이 있을 경우 Func
-            // -> 반환 타입이 없으면 Actional
-            // 무명 함수 / 익명 함수
-            Item item = FindItem(selector);
+                // int c = 0;
 
+                throw new TestException();
+            }
+            catch (DivideByZeroException e)
+            {
+
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+
+            }
         }
 
     }
