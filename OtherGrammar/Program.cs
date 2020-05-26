aff98bb43fe4ff1bd18a1f38c8b1cd2c0d409d02
audidas <audidas@naver.com>
2020-05-26 15:15:33 +0900

    PreDelegate Fuction

--- OtherGrammar/Program.cs
+++ OtherGrammar/Program.cs
@@ -28,8 +28,8 @@ namespace OtherGrammar
     {
         static List<Item> items = new List<Item>();
 
-        delegate bool ItemSelector(Item item);
-        static Item FindItem(ItemSelector selector)
+        //delegate Return MyFunc<T, Return>(T item);
+        static Item FindItem(Func<Item, bool> selector)
         {
             foreach (var item in items)
             {
@@ -47,13 +47,12 @@ namespace OtherGrammar
             items.Add(new Item() { ItemType = ItemType.Armor, Rarity = Rarity.Uncommon });
             items.Add(new Item() { ItemType = ItemType.Ring, Rarity = Rarity.Rare });
 
-            ItemSelector selector = new ItemSelector((Item _item) => { return _item.Rarity == Rarity.Rare; });
-
+            Func<Item, bool> selector = (Item _item) => { return _item.Rarity == Rarity.Rare; };
+            // delegate 를 직접 선언하지 않아도 , 이미 만들어진 애들이 존재한다
+            // -> 반환 타입이 있을 경우 Func
+            // -> 반환 타입이 없으면 Actional
             // 무명 함수 / 익명 함수
-            Item item = FindItem((Item _item) =>
-            {
-                return _item.ItemType == ItemType.Weapon;
-            });
+            Item item = FindItem(selector);
 
         }
 
